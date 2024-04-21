9612dc7de45c2dc8c93bcd2cca6d57222628d4c0
Daniel Álamo Reyes <49496914+Crow6093@users.noreply.github.com>
2024-04-21 18:05:36 +0100

    Add files via upload

---
+++ dist/app.controller.d.ts
@@ -0,0 +1,10 @@
+import { AppService } from './app.service';
+export declare class AppController {
+    private readonly appService;
+    constructor(appService: AppService);
+    googleAuth(req: any): Promise<void>;
+    googleAuthRedirect(req: any): "Failed to login With Google" | {
+        message: string;
+        user: any;
+    };
+}
---
+++ dist/app.service.d.ts
@@ -0,0 +1,6 @@
+export declare class AppService {
+    googleLogin(req: any): "Failed to login With Google" | {
+        message: string;
+        user: any;
+    };
+}
